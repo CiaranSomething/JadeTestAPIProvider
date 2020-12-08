a5fb0570a44e294d1cc050208baef0422adadb84
Ciaran Corrigan <ciaran_corrigan@hotmail.com>
2020-12-08 18:36:22 +0000

    Change to ValuesController

--- JadeTestAPIProvider/Controllers/ValuesController.cs
+++ JadeTestAPIProvider/Controllers/ValuesController.cs
@@ -18,7 +18,7 @@ namespace JadeTestAPIProvider.Controllers
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return $"value: {id}";
         }
 
         // POST api/values
